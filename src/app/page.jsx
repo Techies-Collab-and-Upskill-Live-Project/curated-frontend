56543af152c2e4d4304667c9f394cacc38344a6f
Asak <ose.asak@gmail.com>
2025-05-18 22:04:44 +0100

    Update main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar"; 
+import Searchbar from "@/components/Searchbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className=""> This is the main page of the app</main>
+      <main className="max-w-7xl mx-auto px-6 py-16">
+        <div className="flex flex-col items-center text-center">
+          <h1 className="text-5xl font-bold text-gray-900 mb-6 leading-tight">
+            Discover Smarter Ways to Learn
+          </h1>
+          <p className="text-l text-gray-600 mb-10 max-w-2xl">
+            CuratED helps you find insightful, educational videos tailored to your interests
+          </p>
+          <Searchbar />
+
+        </div>      
+      </main>
     </>
   );
 }
