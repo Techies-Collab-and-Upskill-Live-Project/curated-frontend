3c60b48771d4d516ee69973c876e3d76fd010d63
Asak <ose.asak@gmail.com>
2025-05-22 17:12:26 +0100

    refractor: added the new bulb image
    refractor: changed search color icon

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -16,11 +16,11 @@ export default function Searchbar({ onSearch }) {
       <div className="flex items-center border rounded-[10px] gap-2 mb-2">
         {/* Input Container with Icon */}
         <div className="relative flex-1">
-          <IconSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-900" />
+          <IconSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white" />
           <input
             type="text"
             placeholder="Search/Playlist Link..."
-            className="w-full pl-10 pr-4 py-2 focus:outline-none bg-transparent"
+            className="w-full pl-10 pr-4 py-2 text-white focus:outline-none bg-transparent"
             required
             value={query}
             onChange={(e) => setQuery(e.target.value)}
