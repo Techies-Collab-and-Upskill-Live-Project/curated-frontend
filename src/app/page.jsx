3e53076b3238c243de64602f3edf3395925e6c7a
Asak <ose.asak@gmail.com>
2025-05-22 16:43:09 +0100

    refractor: linked footer to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,20 +2,26 @@
 import Navbar from "@/components/Navbar";
 import Herosection from "@/components/Herosection";
 import Searchbar from "@/components/Searchbar";
+import Footer from "@/components/Footer";
 // import Videotopicgrid from "@/components/Videotopicgrid";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="max-w-7xl mx-auto  px-6 py-16">
-        <Herosection />
+      <header className="max-w-[100%] mx-auto  px-6 py-16 bg-black">
+        
         <div className="flex flex-col items-center text-center">
+          <Herosection />
           <Searchbar />
 
           {/* <Videotopicgrid /> */}
-        </div>
-      </main>
+        </div>        
+      </header>
+
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
