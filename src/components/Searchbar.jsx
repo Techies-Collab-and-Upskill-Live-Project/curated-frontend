cfd82ec3344d9d8c0090c5dc7ccef3dca444295f
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-05-28 22:48:38 +0100

    update search section

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -13,14 +13,14 @@ export default function Searchbar({ onSearch }) {
 
   return (
     <div className="w-full max-w-2xl relative">
-      <div className="flex items-center gap-2 mb-2">
+      <form className="flex items-center text-white border rounded-[10px] gap-2 mb-2" onSubmit={handleSubmit}>
         {/* Input Container with Icon */}
-        <div className="relative flex-1 border rounded-[10px]">
-          <IconSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white" />
+        <div className="relative flex-1">
+          <IconSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 " />
           <input
             type="text"
             placeholder="Search/Playlist Link..."
-            className="w-full pl-10 pr-4 py-2 text-white focus:outline-none bg-transparent"
+            className="w-full pl-10 pr-4 py-2 focus:outline-none bg-transparent"
             required
             value={query}
             onChange={(e) => setQuery(e.target.value)}
@@ -28,10 +28,10 @@ export default function Searchbar({ onSearch }) {
         </div>
 
         {/* Search Button */}
-        <button onClick={handleSubmit} className="bg-primary text-white px-6 py-2 border border-primary rounded-md transition-colors">
+        <button type='submit' className="bg-primary text-white px-6 py-2 rounded-md transition-colors">
           Search
         </button>
-      </div>
+      </form>
     </div>
   );
 }
