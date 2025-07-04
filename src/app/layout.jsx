f7f8e0c19a23f8e33ecd1474b99906bbc2fdbc3a
Ask4mike <mikaelakpang@gmail.com>
2025-07-04 13:57:18 +0100

    refactor: updated the brand icon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,12 +25,20 @@ export const metadata = {
     icon: [
       { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      {
+        url: "/favicon/andriod-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/favicon/andriod-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+
       { url: "/favicon/favicon.ico" },
     ],
     apple: "/favicon/apple-touch-icon.png",
-  },
-  images: {
-    image: "public/assets/images/Curated.png",
   },
   manifest: "/favicon/site.webmanifest",
 };
