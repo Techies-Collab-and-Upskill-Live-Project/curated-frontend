3b74d8a399316ee300d08d2facf2b231a9e1a9da
Ask4mike <mikaelakpang@gmail.com>
2025-07-11 00:17:58 +0100

    refactor: updated reset password with api endpoint , added aria-label to reset modal.

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,17 +1,21 @@
 import api from "./axios";
 
-export const login = (payload) => api.post("/users/login/", payload).then((res)=> res.data);
+export const login = (payload) =>
+  api.post("/users/login/", payload).then((res) => res.data);
 
 export const signup = (userData) => api.post("/users/register/", userData);
-
-export const resetPassword = (email) =>
-  api.post("/auth/reset-password", { email });
 
 export const verifyEmailCode = (payload) =>
   api.post("/users/verify-otp/", payload);
 
 export const resendVerificationCode = (email) =>
   api.post("/users/resend-verification/", { email });
+
+export const resetPassword = (email) =>
+  api.post("/users/password-reset/", { email });
+
+export const resetPasswordConfirm = (payload) =>
+  api.post("/users/password-reset/confirm/", payload);
 
 export const changePassword = (currentPassword, newPassword) =>
   api.put("/users/password/change", currentPassword, newPassword); // Or actual backend route
